5a00d9ab93c5e4effda1e64178ebabdb1dcbab58
SapirBashan <sapirbashan1@gmail.com>
2023-10-31 17:39:36 +0200

    the finishing of linked list

--- src/Windows/LinkedList.js
+++ src/Windows/LinkedList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LinkedList.css";
 import "../styles.css";
+import TogglePopup from "../components/TogglePopup.js";
 
 class Node {
   constructor(data) {
@@ -24,10 +25,10 @@ class LinkedList extends React.Component {
   }
 
   handleKeyPress(e) {
-   if (e.key === "Enter" && this.state.inputVal !== "") {
-     this.insertNode();
-   }
- }
+    if (e.key === "Enter" && this.state.inputVal !== "") {
+      this.insertNode();
+    }
+  }
 
   insertNode() {
     if (this.state.inputVal === "") {
@@ -38,22 +39,23 @@ class LinkedList extends React.Component {
       alert("Please enter a number");
       return;
     }
-    if(this.state.inputVal > 9999 || this.state.inputVal < -999) {
+    if (this.state.inputVal > 9999 || this.state.inputVal < -999) {
       alert("Please enter a number between -999 and 9999");
       return;
     }
     const newNode = new Node(this.state.inputVal);
     if (!this.state.head) {
-      this.setState({ head: newNode });
-    } 
-    else { 
-         let current = this.state.head;
-         while (current.next) {
-            current = current.next;
-         }
-         current.next = newNode;
+      this.setState({ head: newNode});
+    } else {
+      let current = this.state.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
-    this.setState({ inputVal: "" });
+    setTimeout(() => {
+      this.setState({ inputVal: ""})
+   }); 
   }
 
   deleteNode(value) {
@@ -66,73 +68,81 @@ class LinkedList extends React.Component {
        } else {
          prev.next = current.next;
        }
-       break;
+       this.setState({ inputVal: "" }); // Reset inputVal state to an empty string
+       return;
      }
      prev = current;
      current = current.next;
    }
-   if (current === null) {
-     alert("Node not found");
-   }
+   alert("Node not found");
  }
+ 
+
 
   findNode(value) {
-   let current = this.state.head;
-   while (current !== null) {
-     if (current.data === value) {
-       this.setState({ foundKey: current.data }); // Update foundKey state with the key of the found node
-       return true;
-     }
-     current = current.next;
-   }
-   alert("Node not found");
-   return false;
- }
+    let current = this.state.head;
+    while (current !== null) {
+      if (current.data === value) {
+         this.setState({ foundKey: current.data }); // Update foundKey state with the key of the found node
+        return true;
+      }
+      current = current.next;
+    }
+    alert("Node not found");
+    return false;
+  }
 
- render() {
-   let nodes = [];
-   let arrows = [];
-   let current = this.state.head;
-
-   while (current !== null) {
-     nodes.push(
-       <div
-         key={current.data}
-         className={`block ${this.state.foundKey === current.data ? 'found' : ''}`}
-       >
-         {current.data}
-       </div>
-     );
-     if (current.next !== null) {
-       arrows.push(<div key={current.data + "->"} className="arrow"></div>);
-     }
-     current = current.next;
+  RandomNode(value) {
+      this.setState({ inputVal: Math.floor(Math.random() * 100) + 1})
    }
 
-   return (
+
+  render() {
+    let nodes = [];
+    let arrows = [];
+    let current = this.state.head;
+
+    while (current !== null) {
+      nodes.push(
+        <div
+          key={current.data}
+          className={`block ${this.state.foundKey === current.data ? "found" : ""}`}
+        >
+          {current.data}
+        </div>
+      );
+      if (current.next !== null) {
+        arrows.push(<div key={current.data + "->"} className="arrow"></div>);
+      }
+      current = current.next;
+    }
+
+    return (
       <div className="LinkedList">
-      <h1>Linked List</h1>
-      <input
-        type="text"
-        value={this.state.inputVal}
-        onChange={(e) => this.handleInput(e)}
-        onKeyPress={(e) => this.handleKeyPress(e)}
-      />
-      <button onClick={() => this.deleteNode(this.state.inputVal)}>Delete</button>
-       <button onClick={() => this.insertNode()}>Insert</button>
-       <button onClick={() => this.findNode(this.state.inputVal)}>Find</button>
-       <h1> </h1>
-       <div className="linked-list-container">
-         {nodes.map((node, index) => (
-           <React.Fragment key={index}>
-             {node}
-             {arrows[index]}
-           </React.Fragment>
-         ))}
-       </div>
-     </div>
-   );
- }
+        <h1>Linked List</h1>
+        <input
+          type="text"
+          value={this.state.inputVal}
+          onChange={(e) => this.handleInput(e)}
+          onKeyPress={(e) => this.handleKeyPress(e)}
+        />
+        <button onClick={() => this.deleteNode(this.state.inputVal)}>Delete</button>
+        <button onClick={() => this.insertNode()}>Insert</button>
+        <button onClick={() => this.findNode(this.state.inputVal)}>Find</button>
+        <button onClick={() => this.RandomNode(this.state.inputVal)}>Random</button>
+        <h1> </h1>
+        <div className="linked-list-container">
+          {nodes.map((node, index) => (
+            <React.Fragment key={index}>
+              {node}
+              {arrows[index]}
+            </React.Fragment>
+          ))}
+        </div>
+        <TogglePopup />
+      </div>
+    );
+  }
 }
 
 export default LinkedList;
