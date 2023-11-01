418139a26abe9c85aa6ac33d4651e382c263f2cb
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-11-01 17:49:54 +0200

    node

--- src/Components/Node.js
+++ src/Components/Node.js
@@ -2,21 +2,19 @@ import React from 'react';
 import './Node.css';
 
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-
-  constructor(data, next) {
-    Node(data);
-    this.next = next;
+  constructor(props) {
+    super(props);
+    this.state = {
+      pointers: [null],
+      data: props.data,
+    };
   }
 
   render() {
     return (
       <div className="Node">
-        <div className='Node-data'>{this.data}</div>
         <div className='Node-next'>&rarr;</div> {/* Use the Unicode right arrow character (→) */}
+        <div className='Node-data'>{this.state.data}</div>
       </div>
     );
   }
