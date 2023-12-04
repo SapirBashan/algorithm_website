eac9cab786505eb5cfb86f6ce33138f759be1c78
SapirBashan <sapirbashan1@gmail.com>
2023-12-04 12:52:26 +0200

    chnges

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from '../src/windows/Home.js';
+import Home from './windows/Home.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
